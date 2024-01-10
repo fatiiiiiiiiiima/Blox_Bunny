a0f69b1a782787c3dd51669e078bc1bacb3e5c9d
Hadia Noor <h.noor@prismware.co>
2024-01-11 00:07:35 +0500

    mobile responsive

--- src/app/components/layout/layout.js
+++ src/app/components/layout/layout.js
@@ -4,7 +4,9 @@ import './globals.css'
 const Layout = ({ children }) => {
   return (
     <div className="app-layout">
+      {/* <div className='sidebararea'> */}
       <Sidebar />
+      {/* </div> */}
       <div className="content">{children}</div>
     </div>
   );
